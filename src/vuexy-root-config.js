fea179bed0857c735b88c6b32e0d78324c9b6021
Mahmoud.yasser <Mahmoud.yasser@tds-me.com>
2023-08-05 12:17:51 +0200

    Modify

--- src/vuexy-root-config.js
+++ src/vuexy-root-config.js
@@ -1,20 +1,12 @@
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
+registerApplication({
+  name: "@navbar/vuexy-navbar",
+  app: () => System.import("@navbar/vuexy-navbar"),
+  activeWhen: ["/"],
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
-start();
+start({
+  urlRerouteOnly: true,
+  timeout: 10000,
+});
